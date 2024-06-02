4fdcc685f5aa7e7b9739042589d6db64984c5e65
RelouBird <ulrichkwamou@gmail.com>
2024-06-02 19:55:51 +0100

    tfy

--- js/firebasInit.js
+++ js/firebasInit.js
@@ -3,15 +3,14 @@ import { initializeApp } from "https://www.gstatic.com/firebasejs/10.12.2/fireba
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-    apiKey: "AIzaSyDlo9AyTEkgZcKv2Iz71fRJC_86hlOsICU",
-    authDomain: "voted-web-app.firebaseapp.com",
-    projectId: "voted-web-app",
-    storageBucket: "voted-web-app.appspot.com",
-    messagingSenderId: "408819279527",
-    appId: "1:408819279527:web:9afdbd2dd82f72059f5d82",
-    measurementId: "G-GY91LC26CJ",
+    apiKey: "AIzaSyBf4Z-5S7VhFmIACU0o2G2qCd5BOqj_03o",
+    authDomain: "voted-app.firebaseapp.com",
+    projectId: "voted-app",
+    storageBucket: "voted-app.appspot.com",
+    messagingSenderId: "594677514204",
+    appId: "1:594677514204:web:666c10c0d7ea3f71fe1919",
+    measurementId: "G-2R6BPTNJB3"
   };
-
   
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
--- script.js
+++ script.js
@@ -13,65 +13,68 @@ import {
 
 import app from "./js/firebasInit.js";
 
-
-
 const auth = getAuth(app);
 
 var provider = new GoogleAuthProvider();
 
+
+
 function SignInMobile() {
-  signInWithRedirect(auth,provider);
+  signInWithRedirect(auth, provider);
   getRedirectResult(auth)
-  .then((result) => {
-    if (result.credential) {
-      window.location.assign("./pages/home.html");
-    }
-  }).catch((error) => {
-    // Handle Errors here.
-    var errorCode = error.code;
-    var errorMessage = error.message;
-    // The email of the user's account used.
-    var email = error.email;
-    // The firebase.auth.AuthCredential type that was used.
-    var credential = error.credential;
-    // ...
-  });
+    .then((result) => {
+      if (result.credential) {
+        window.location.assign('./home.html')
+      }
+    })
+    .catch((error) => {
+      // Handle Errors here.
+      var errorCode = error.code;
+      var errorMessage = error.message;
+      // The email of the user's account used.
+      var email = error.email;
+      // The firebase.auth.AuthCredential type that was used.
+      var credential = error.credential;
+      // ...
+    });
 }
 
+async function signInPc() {
+  await signInWithPopup(auth, provider)
+    .then((result) => {
 
-async function signInPc (){
-  await signInWithPopup(auth,provider).then((result) => {
-    /** @type {firebase.auth.OAuthCredential} */
-    var credential = result.credential;
+      /** @type {firebase.auth.OAuthCredential} */
+      var credential = result.credential;
 
-    // This gives you a Google Access Token. You can use it to access the Google API.
-    var token = credential.accessToken;
-    // The signed-in user info.
-    var user = result.user;
-    // IdP data available in result.additionalUserInfo.profile.
+      // This gives you a Google Access Token. You can use it to access the Google API.
+      var token = credential.accessToken;
+      // The signed-in user info.
+      var user = result.user;
+      // IdP data available in result.additionalUserInfo.profile.
       // ...
-      window.location.assign("./pages/home.html");
-  }).catch((error) => {
-    // Handle Errors here.
-    var errorCode = error.code;
-    var errorMessage = error.message;
-    // The email of the user's account used.
-    var email = error.email;
-    // The firebase.auth.AuthCredential type that was used.
-    var credential = error.credential;
-    // ...
-  })
+      // window.location.assign("./pages/home.html");
+      window.location.assign('./home.html')
+    })
+    .catch((error) => {
+      // Handle Errors here.
+      var errorCode = error.code;
+      var errorMessage = error.message;
+      // The email of the user's account used.
+      var email = error.email;
+      // The firebase.auth.AuthCredential type that was used.
+      var credential = error.credential;
+      // ...
+    });
 }
 
 var googleButton = document.querySelector(".but.but1");
 var appleButton = document.querySelector(".but.but2");
 
-googleButton.addEventListener("click", ()=>{
+googleButton.addEventListener("click", () => {
   window.screen.width < 500 ? SignInMobile() : signInPc();
 });
 
-
-appleButton.addEventListener("click", async ()=>{
+appleButton.addEventListener("click", async () => {
   await signOut(auth);
-  console.log("sign out successfuly.")
+  console.log("sign out successfuly.");
 });
