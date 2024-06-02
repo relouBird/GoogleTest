e89c5a99dbb6df14a9a5e8c67e69562a7e6069eb
RelouBird <ulrichkwamou@gmail.com>
2024-06-02 22:10:55 +0100

    actually reload

--- script.js
+++ script.js
@@ -39,32 +39,36 @@ function SignInMobile() {
     });
 }
 
-async function signInPc() {
-  await signInWithPopup(auth, provider)
-    .then((result) => {
+// async function signInPc() {
+//   await signInWithPopup(auth, provider)
+//     .then((result) => {
 
-      /** @type {firebase.auth.OAuthCredential} */
-      var credential = result.credential;
+//       /** @type {firebase.auth.OAuthCredential} */
+//       var credential = result.credential;
 
-      // This gives you a Google Access Token. You can use it to access the Google API.
-      var token = credential.accessToken;
-      // The signed-in user info.
-      var user = result.user;
-      // IdP data available in result.additionalUserInfo.profile.
-      // ...
-      // window.location.assign("./pages/home.html");
-      window.location.assign('./home.html')
-    })
-    .catch((error) => {
-      // Handle Errors here.
-      var errorCode = error.code;
-      var errorMessage = error.message;
-      // The email of the user's account used.
-      var email = error.email;
-      // The firebase.auth.AuthCredential type that was used.
-      var credential = error.credential;
-      // ...
-    });
+//       // This gives you a Google Access Token. You can use it to access the Google API.
+//       var token = credential.accessToken;
+//       // The signed-in user info.
+//       var user = result.user;
+//       // IdP data available in result.additionalUserInfo.profile.
+//       // ...
+//       // window.location.assign("./pages/home.html");
+//       window.location.assign('./home.html')
+//     })
+//     .catch((error) => {
+//       // Handle Errors here.
+//       var errorCode = error.code;
+//       var errorMessage = error.message;
+//       // The email of the user's account used.
+//       var email = error.email;
+//       // The firebase.auth.AuthCredential type that was used.
+//       var credential = error.credential;
+//       // ...
+//     });
+// }
+
+function signInPc (){
+  window.location.assign("./pages/home.html")
 }
 
 var googleButton = document.querySelector(".but.but1");
